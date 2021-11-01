91c09476f85d8389cee4745045d6c059d46e0273
diksha1012 <trivedidiksha357@gmail.com>
2021-11-01 15:00:19 +0530

    changes in url

--- myEcom/urls.py
+++ myEcom/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
-from . import settings
+from django.conf import settings
+# from . import settings
 from django.views.static import serve
 from django.conf.urls import url
 
@@ -24,16 +25,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls')),
     path('index',include('shop.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     # path('shop/base',include('shop.urls')),
    
 
 ]  
+if settings.DEBUG:
 
-if settings.debug:
-
- urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
- urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
