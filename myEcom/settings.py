7ee4698195a30f8a2841cdb32e0affcb2129e1d0
diksha1012 <trivedidiksha357@gmail.com>
2021-11-01 15:20:58 +0530

    changes in settings

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -172,7 +172,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
 MEDIA_URL = "/uploads/products/"
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT=BASE_DIR
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"shop/static/images"),
