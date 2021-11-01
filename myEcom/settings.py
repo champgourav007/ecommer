cbeca509206b822b6498138d585591389d8aa101
diksha1012 <trivedidiksha357@gmail.com>
2021-11-01 14:40:39 +0530

    changes in settings

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -167,7 +167,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # MEDIA_ROOT=os.path.join(BASE_DIR,"uploads/products/")
 
-MEDIA_ROOT=BASE_DIR
+MEDIA_ROOT=os.path.join(BASE_DIR,"shop/static/images")
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'
