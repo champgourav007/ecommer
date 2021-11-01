0b0537005709ca732fadeb6c0ea07d1dc038b276
diksha1012 <trivedidiksha357@gmail.com>
2021-11-01 15:15:41 +0530

    changes in settings

--- myEcom/urls.py
+++ myEcom/urls.py
@@ -25,15 +25,15 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('shop.urls')),
     path('index',include('shop.urls')),
-    # url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
     # path('shop/base',include('shop.urls')),
    
 
 ]  
-if settings.DEBUG:
+# if settings.DEBUG:
 
-    urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+#     urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
