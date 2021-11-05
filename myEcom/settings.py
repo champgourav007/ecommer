e46d79bf34d6bad23d6d7b9d30814400672911bd
diksha1012 <trivedidiksha357@gmail.com>
2021-11-05 13:25:16 +0530

    changes in settings

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -193,7 +193,7 @@ STATICFILES_DIRS = [
 # STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 MEDIA_URL = "/uploads/products/"
 MEDIA_ROOT=BASE_DIR
