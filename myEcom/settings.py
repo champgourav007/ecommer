824faeace6c781e8987dae1e475ee74b31bb402b
diksha1012 <trivedidiksha357@gmail.com>
2021-11-05 13:54:06 +0530

    changes in index

--- myEcom/settings.py
+++ myEcom/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-# import django_heroku
+import django_heroku
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myEcom.settings")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -193,10 +193,10 @@ STATICFILES_DIRS = [
 # STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-# django_heroku.settings(locals())
 
 MEDIA_URL = "/uploads/products/"
 MEDIA_ROOT=BASE_DIR
+django_heroku.settings(locals())
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
