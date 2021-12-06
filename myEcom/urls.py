60f25dbe170f694a6b9aeb1de7e4ee3b4306748f
Gourav Kumar Singh <gourav.19b131001@abes.ac.in>
2021-12-06 10:22:04 +0530

    urlpatterns media url

--- myEcom/urls.py
+++ myEcom/urls.py
@@ -30,8 +30,8 @@ urlpatterns = [
 
     # path('shop/base',include('shop.urls')),
 ]
-urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns=urlpatterns+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns=urlpatterns+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
    
 
